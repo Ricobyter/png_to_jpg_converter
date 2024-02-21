e7cffe731c1fe14d326ac2e6bd41b7a3a3545433
Dhruv <dhruvanandsingh2004@gmail.com>
2024-02-21 21:21:43 +0530

    added all functionalities

--- src/App.js
+++ src/App.js
@@ -43,38 +43,37 @@ export default function App()  {
     };
 
     return (
-        <div className="container h-[100vh] flex items-center w-[100vw] justify-center flex-col bg-[#030637]
-        ">
-            <nav className="bg-crimson mt-4 text-red-700 font-bold text-center text-2xl md:text-3xl uppercase rounded">
+        <div className="container h-full flex items-center w-[100vw] justify-center flex-col bg-[#030637]">
+            <nav className="bg-crimson mt-4 text-red-600 font-bold text-center text-xl md:text-2xl uppercase rounded">
                 <h1>PNG to JPG Converter</h1>
             </nav>
 
             <div className='h-[40vh] mb-3 '>
-            <label htmlFor="input" className="flex flex-col items-center mt-10 border-dashed w-[80vw] md:w-[60vw] border-white rounded-xl h-full">
+            <label htmlFor="input" className="flex flex-col items-center mt-10 w-[80vw] md:w-[60vw] rounded-lg h-full">
               
-                <span className="text-purple-500 text-lg font-bold mt-4">Drop files here</span>
-                <div className='w-[50vw] overflow-hidden my-auto'>
+                <span className="text-purple-500 text-lg font-bold mt-4 cursor-pointer">Click here or Drop files here</span>
+                <div className='w-[60vw] md:w-[50vw] line-clamp-1 my-auto'>
 
-                {fileName &&( <span className="text-purple-500 flex gap-2 "><FaImage/>{fileName}</span>)}
+                {fileName &&( <span className="text-pink-500 flex gap-2 items-center justify-center"><FaImage/>{fileName}</span>)}
                 </div>
                 <input type="file" id="input" accept="image/png" onChange={handleFileChange} className="hidden" />
             </label>
             </div>
 
-            <button className="mt-10 bg-crimson text-xl text-white  w-[80vw] md:w-[60vw] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  rounded-xl py-2 px-4   transition duration-300" onClick={convertImage}>
+            <button className="mt-10 bg-crimson text-xl text-white  w-[80vw] md:w-[60vw] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  rounded-xl py-2 px-4 font-semibold" onClick={convertImage}>
                 Click Here to Download JPG
             </button>
            <div className='flex justify-center items-center'>
 
 
-            <div className="mt-10 w-[80vw] md:w-[60vw] text-white">
-                <h2 className="text-center text-crimson font-bold uppercase mb-4">About PNG to JPG Converter</h2>
-                <p>A website for the best PNG to JPG converter is a platform that allows users to easily convert their PNG images to the JPG format. The PNG to JPG conversion process is essential for users who want to optimize their image files for web use, where JPG is the preferred format</p>
-                <p>The website features a simple upload system, where users can select their PNG image file and initiate the conversion process with a single click. The conversion process is fast and efficient, producing high-quality JPG images that maintain the visual integrity of the original PNG files.</p>
+            <div className="mt-10 w-[80vw] md:w-[60vw] text-white text-center">
+                <h2 className="text-center text-crimson font-bold uppercase mb-4 text-purple-600 text-lg">About PNG to JPG Converter</h2>
+                <p >A website for the best PNG to JPG converter is a platform that allows users to easily convert their PNG images to the JPG format. The PNG to JPG conversion process is essential for users who want to optimize their image files for web use, where JPG is the preferred format</p>
+                <p className='mt-2 hidden md:block'>The website features a simple upload system, where users can select their PNG image file and initiate the conversion process with a single click. The conversion process is fast and efficient, producing high-quality JPG images that maintain the visual integrity of the original PNG files.</p>
             </div>
            </div>
 
-            <footer className="text-center bottom-2 mt-6 text-white">Copyright &copy; <a href="#">Rico</a> | Developed With you ❤️ By <a href="#" className='text-pink-500'>Rico</a></footer>
+            <footer className="text-center bottom-2 mt-6 text-gray-300 text-xs">Copyright &copy; <a href="#">Rico</a> | Developed With you ❤️ By <a href="#" className='text-pink-500'>Rico</a></footer>
         </div>
     );
 };
